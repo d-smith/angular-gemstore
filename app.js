362f5e332d0e272926d9b9d53e55ecc36c4a16ba
d-smith <doug.smith.mail@gmail.com>
2014-08-08 13:39:10 -0600

    Re-baselined prior to start of forms module

--- app.js
+++ app.js
@@ -8,11 +8,11 @@
 	app.controller('PanelController', function() {
 		this.tab = 1;
 
-		this.selectTab = function(setTab) {
+		this.setCurrent = function(setTab) {
 			this.tab = setTab;
 		};
 
-		this.isSelected = function(checkTab) {
+		this.isCurrent = function(checkTab) {
 			return this.tab === checkTab;
 		};
 	});
