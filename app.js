a9294b1a73ce83e32b3180d70d519b1c3e915a97
d-smith <doug.smith.mail@gmail.com>
2014-08-08 14:54:50 -0600

    Fixed up form presentation

--- app.js
+++ app.js
@@ -22,6 +22,10 @@
 		name: 'Dodecahedron',
 		price: 2,
 		description: "Oh, so pretty.",
+		reviews: [
+			{ body: "I love it!", stars: 5, author: "fanboy@apple.com"},
+			{body: "Meh", stars: -2, author: "grumpy@grumpycat.com"}
+		],
 		canPurchase: true,
 		soldOut: false
 	},
@@ -29,6 +33,10 @@
 		name: 'Pentagonal Gem',
 		price: 5.95,
 		description: "A rare gem with 5 facets",
+		reviews: [
+			{ body: "I love it!", stars: 5, author: "fanboy@apple.com"},
+			{body: "Meh", stars: -2, author: "grumpy@grumpycat.com"}
+		],
 		canPurchase: false,
 		soldOut: false
 	}
